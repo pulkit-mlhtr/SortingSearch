db87a6889f62d702cce15d7befd3a8e15c60beb4
pulkit-mlhtr <pulkit_mlhtr@yahoo.com>
2023-09-23 21:07:08 +0200

    Pushing Binary Search

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace CountNumber
+{
+    public static class SortedSearch
+    {
+
+        public static int CountNumbers(int[] sortedArray, int lessThan)
+        {                        
+
+            return GetCount(sortedArray,lessThan,0,sortedArray.Length-1);
+        }
+
+
+        private static int GetCount(int[] sortedArray, int lessThan, int min, int max)
+        {      
+            //Handles the scenrio when pointer moves towards extreme right or left
+            if (min>max && min <= sortedArray.Length)
+            {
+                if(max > -1 && sortedArray[max] == lessThan)
+                {
+                    return max;
+                } 
+                else
+                {
+                    return min;
+                }                
+            }           
+            else
+            {                
+                int mid = (min + max) / 2;
+
+                if(lessThan == sortedArray[min])
+                {
+                    //Return if exact value found in the array
+                    return min;
+                }
+               
+                if (lessThan < sortedArray[mid])
+                {
+                    //Move towards left 
+                    return GetCount(sortedArray, lessThan, min, mid - 1);
+                }
+                else
+                {
+                    //Move towards right
+                    return GetCount(sortedArray, lessThan, mid + 1, max);
+                }
+            }            
+        }       
+
+        public static void Main(string[] args)
+        {
+            //Change input here
+            Console.WriteLine(SortedSearch.CountNumbers(new int[] { 1, 3, 5, 7,8,9,10,11,15,16,18,19 }, 6));
+        }
+    }
+}
